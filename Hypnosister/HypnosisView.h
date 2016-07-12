71667b897c63943af7355a7320b4e30d63ab89b3
Karan Jivani <karanjivani@gmail.com>
2016-07-12 16:06:49 -0500

    Added Hypnosis view class, Subclass of UIView

---
+++ Hypnosister/HypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  HypnosisView.h
+//  Hypnosister
+//
+//  Created by Karan Jivani on 7/12/16.
+//  Copyright © 2016 Karan Jivani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisView : UIView
+
+@end
---
+++ Hypnosister/HypnosisView.m
@@ -0,0 +1,21 @@
+//
+//  HypnosisView.m
+//  Hypnosister
+//
+//  Created by Karan Jivani on 7/12/16.
+//  Copyright © 2016 Karan Jivani. All rights reserved.
+//
+
+#import "HypnosisView.h"
+
+@implementation HypnosisView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
